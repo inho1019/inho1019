4365aed5a81333c3c1ae8dca925b2dd7ffa6d6e4
inho1019 <inho1019@naver.com>
2025-07-08 16:02:02 +0900

    test

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ Spring Boot를 통한 백엔드 개발과 ERD 설계를 통한 데이터베이�
 
 <br>
 <div align="center">
-  <img height="180em" src="https://github-readme-stats.vercel.app/api?username=inho1019&theme=radical?count_private=true" />&nbsp;
-  <img height="180em" src="https://github-readme-stats.vercel.app/api/top-langs/?username=inho1019&layout=compact&theme=radical?count_private=true" />
+  <img height="180em" src="https://github-readme-stats.vercel.app/api?username=inho1019&theme=radical&count_private=true" />&nbsp;
+  <img height="180em" src="https://github-readme-stats.vercel.app/api/top-langs/?username=inho1019&layout=compact&theme=radical&count_private=true" />
 </div>
 
 ## Posts
